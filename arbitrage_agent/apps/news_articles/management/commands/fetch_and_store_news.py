3918952f1a1a20b93512e9e62c72d7c846a6c95e
GTerrence <g.terrencep101@gmail.com>
2026-01-18 17:23:42 +0700

    Add management commands

---
+++ arbitrage_agent/apps/news_articles/management/commands/fetch_and_store_news.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandParser
+from arbitrage_agent.apps.news_articles.utils import fetch_and_store_news
+
+class Command(BaseCommand):
+    help = 'Fetches news from RSS and stores them'
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            '--batch-size',
+            type=int,
+            default=20,
+            help='Number of articles to fetch and store in one batch'
+        )
+
+    def handle(self, *args, **options):
+        # The command just triggers the logic
+        fetch_and_store_news(batch_size=options['batch_size'])
+        self.stdout.write(self.style.SUCCESS('Command initiated successfully'))
