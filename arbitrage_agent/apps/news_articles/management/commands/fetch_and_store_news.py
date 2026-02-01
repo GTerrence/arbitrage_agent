c52455a69fda1a31989a882799cf37371ecee075
GTerrence <57930152+GTerrence@users.noreply.github.com>
2026-02-01 12:13:30 +0700

    feat: Mock Seed Data (#8)
    * Add seed data function
    
    * Fix lint
    
    * Minor fix

--- arbitrage_agent/apps/news_articles/management/commands/fetch_and_store_news.py
+++ arbitrage_agent/apps/news_articles/management/commands/fetch_and_store_news.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.core.management.base import BaseCommand, CommandParser
 
 from arbitrage_agent.apps.news_articles.utils import fetch_and_store_news
@@ -14,7 +16,7 @@ class Command(BaseCommand):
             help='Number of articles to fetch and store in one batch'
         )
 
-    def handle(self, *args, **options):
+    def handle(self, *args: Any, **options: Any) -> None:
         # The command just triggers the logic
         fetch_and_store_news(batch_size=options['batch_size'])
         self.stdout.write(self.style.SUCCESS('Command initiated successfully'))
