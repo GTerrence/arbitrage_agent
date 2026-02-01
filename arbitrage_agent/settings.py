3eebc9e4d0fef05030e1054e264f5bfc2425fae6
GTerrence <g.terrencep101@gmail.com>
2026-02-01 10:31:18 +0700

    Update docker setup

--- arbitrage_agent/settings.py
+++ arbitrage_agent/settings.py
@@ -13,7 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-14t^cyffysg$^r-f-_y!yukskxv35os%85v2ft&m2oz^^jui*n"
+DEFAULT_SECRET_KEY = "django-insecure-14t^cyffysg$^r-f-_y!yukskxv35os%85v2ft&m2oz^^jui*n"
+SECRET_KEY = os.getenv("SECRET_KEY", DEFAULT_SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
